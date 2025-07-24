1786266cc004f893e8ebe822961ae241f8e879c0
atabzo <myemail.bhanu@gmail.com>
2025-07-24 20:46:32 +0530

    adding text on the homepage

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="min-h-screen w-full bg-[#a1161f] relative">
     <!-- Main content goes here -->
-    <img src="/kiki_red.png" alt="Kiki's Cat" class="absolute bottom-0 right-0 w-40 h-40 object-contain" />
+    <img src="/kiki_red.png" alt="Kiki's Cat" class="absolute bottom-0 right-0 w-32 md:w-44 h-auto object-contain" />
+    <h1 class="text-white text-3xl md:text-5xl font-bold p-8">
+      Kiki's Compression Service!
+    </h1>
   </div>
 </template>
