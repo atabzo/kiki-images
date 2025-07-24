22cb935d3802469a6a6a6939da1919a673122aaf
atabzo <myemail.bhanu@gmail.com>
2025-07-24 20:39:23 +0530

    adding graphics on the homepage

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,6 @@
 <template>
-  <div>
-    <h1 class="font-barrio text-4xl"> Kiki Compression Service </h1>
+  <div class="min-h-screen w-full bg-[#a1161f] relative">
+    <!-- Main content goes here -->
+    <img src="/kiki_red.png" alt="Kiki's Cat" class="absolute bottom-0 right-0 w-40 h-40 object-contain" />
   </div>
 </template>
